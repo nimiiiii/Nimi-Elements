b9a294d3397007c1f9a58c17bb4ba5303898bbc4
tommywalkie <tommywalkie@gmail.com>
2020-10-02 14:27:11 +0200

    Apply new LICENSE in existing source code

--- packages/nimi-icons/test/blah.test.tsx
+++ packages/nimi-icons/test/blah.test.tsx
@@ -1,6 +1,6 @@
 /*
  * Copyright 2020 Tom Bazarnik et al.
- * Licensed under the GNU General Public License v3
+ * Licensed under the GNU General Public License v2.0 w/Classpath exception
  * See LICENSE for details.
  */
 
